e4580c4ba5165c013b7e4396c742636583b285f0
tsounguc <tsounguc@mail.gvus.edu>
2019-08-23 11:35:38 -0400

    first commit

---
+++ app/src/main/java/mail/gvsu/edu/myfirstgame/AboutMe.kt
@@ -0,0 +1,12 @@
+package mail.gvsu.edu.myfirstgame
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AboutMe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_me)
+    }
+}
---
+++ app/src/main/java/mail/gvsu/edu/myfirstgame/MainActivity.kt
@@ -0,0 +1,27 @@
+package mail.gvsu.edu.myfirstgame
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log.d
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        bigButton.setOnClickListener {
+            d("christian", "button was pressed!")
+            startActivity(Intent(this, AboutMe::class.java))
+        }
+
+    }
+}
